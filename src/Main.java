4c8ff48180f14a068a6b9d25dfa7080abcd0055b
Gokul1909 <gokul@terzocloud.com>
2023-06-13 13:17:52 +0530

    Escape sequence

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,15 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        //String
+        //Escape sequence
 
-        //The classes in java.lang need not be imported
-        String name="Gokul"; //Short-hand notation to declare a string object as it is most commonly used class
-        System.out.println(name);
-        name=name+".S"; //Concatenation of two string using + operator
-        System.out.println(name);
+        // '\' can be used to escape a special character succeeding it
+        System.out.println("Hello \"Gokul\"");
+        // To escape a backslash itself we can use another preceding it
+        System.out.println("C:\\Documents\\CoreJava.txt");
 
-        //String methods
-        System.out.println(name.replace('.',' ')); //doesn't change the actual string name
-        System.out.println(name);
-        System.out.println(name.toLowerCase());
-        System.out.println(name.toUpperCase());
-        System.out.println(name.length());
-        System.out.println(name.endsWith("S"));
-        name=" Gokul S";
-        System.out.println(name.trim()); //removes spaces before and after string
+        //Other escape sequences commonly used are \n and \t
+        System.out.println("\n\nHi");
+        System.out.println("\t\tHi");
     }
     }
