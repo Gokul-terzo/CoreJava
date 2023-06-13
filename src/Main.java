556b0469fa4e85b58d3f96ec1428145cc5f52684
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:59:52 +0530

    Do While loop

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //While loop
+        //Do-While loop
         Scanner sc=new Scanner(System.in);
         String input="";
-        while(!input.equals("quit")||!input.equals("Quit"))
-        {
-            System.out.println(input);
+        do{
+            System.out.println("Enter the input");
             input=sc.next();
             System.out.println("Input is:"+input);
-        }
+        }while(!input.equals("quit")&&!input.equals("Quit"));
     }
     }
