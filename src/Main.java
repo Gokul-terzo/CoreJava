49abda64be5904e229fd6988734ac3f1f3021578
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:54:51 +0530

    While loop

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //For Loop
-        for (int i=0;i<=5;i++)
-            System.out.println("Hi number is"+i);
+        //While loop
+        Scanner sc=new Scanner(System.in);
+        String input="";
+        while(!input.equals("quit")||!input.equals("Quit"))
+        {
+            System.out.println(input);
+            input=sc.next();
+            System.out.println("Input is:"+input);
+        }
     }
     }
