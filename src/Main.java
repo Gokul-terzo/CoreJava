ff3a990512c9bcd55e5670d700f9de95b4e1b9a9
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:50:50 +0530

    For loop

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,8 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //Fizz Buzz
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the number:");
-        int number=sc.nextInt();
-        if(number%5==0)
-        {
-            if(number%3==0)
-                System.out.println("Fizz Buzz");
-            else
-                System.out.println("Fizz");
-        }
-        else if(number%3==0)
-        {
-            System.out.println("Buzz");
-        }
-        else
-            System.out.println(number);
+        //For Loop
+        for (int i=0;i<=5;i++)
+            System.out.println("Hi number is"+i);
     }
     }
