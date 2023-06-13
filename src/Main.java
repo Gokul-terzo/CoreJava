b0fcccb4e338c31b9675621ecab3517403105c8a
Gokul1909 <gokul@terzocloud.com>
2023-06-13 12:48:36 +0530

    Reference vs Primitive

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
-import java.util.Date;
+import java.awt.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Reference Variable
-        Date date=new Date(); //Creating an instance of Date class
-        System.out.println(date);
+        //Primitive Vs Reference
+
+        //The primitive variables are totally independent of each other.
+        int a=10;
+        int b=a;
+        a=30;
+        System.out.println(b); //Value of B does not change!
+
+        //Reference variables point to the location and hence if a variable is assigned to another,they hold the same value
+
+        Point point1=new Point(1,1);
+        Point point2=point1; //The same address is assigned to point2 object
+        point1.x=5;
+        System.out.println(point2); //The value of point2 is coherent with that of point1.
         }
     }
