ea7fbc78fa474913e31c9933bc69026179e00496
Gokul1909 <gokul@terzocloud.com>
2023-06-13 15:23:31 +0530

    MathClass

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Casting
+        //Math class
+        int n=Math.max(1,2); //Returns the maximum of 1 & 2 to n
+        n=Math.min(1,2);  //Returns the minimum of 1&2
+        System.out.println(n);
+        n=(int)Math.ceil(1.1F); //Explicit conversion to change returned value
+        System.out.println(n);
+        n=(int)Math.floor(1.9F);
+        System.out.println(n);
 
-        //Implicit conversion
-        int x=3;
-        float y=x+1.0F; //java implicitly converts to float
-        System.out.println(y);
-
-        //Explicit conversion
-        float a=12.3F;
-        int b=(int)a+1; //the variable a is converted from float to int
-        System.out.println(b);
-
-        //String to number using classes
-        String number="1";
-        System.out.println(Integer.parseInt(number));
-        String number1="1.12";
-        System.out.println(Double.parseDouble(number1));
+        //Random function
+        double rand=Math.random(); //Returns a floating number from 0-1
+        int m=(int) Math.random()*100; //returns 0 as the whole expression is not converted to int
+        m=(int)(Math.random()*100); //returns value between 1-100 as integer
+        System.out.println(m);
     }
     }
