d77eb63701294ea356680da08b07c55626b29500
Gokul1909 <gokul@terzocloud.com>
2023-06-13 15:56:03 +0530

    Number Formatting

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
-
+import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-        //Math class
-        int n=Math.max(1,2); //Returns the maximum of 1 & 2 to n
-        n=Math.min(1,2);  //Returns the minimum of 1&2
-        System.out.println(n);
-        n=(int)Math.ceil(1.1F); //Explicit conversion to change returned value
-        System.out.println(n);
-        n=(int)Math.floor(1.9F);
-        System.out.println(n);
+        //Number formatting
 
-        //Random function
-        double rand=Math.random(); //Returns a floating number from 0-1
-        int m=(int) Math.random()*100; //returns 0 as the whole expression is not converted to int
-        m=(int)(Math.random()*100); //returns value between 1-100 as integer
-        System.out.println(m);
+        //Currency
+        NumberFormat currency=NumberFormat.getCurrencyInstance(); //cannot use new as it is a abstract class
+        String result=currency.format(30000);
+        System.out.println(result);
+        System.out.println(NumberFormat.getCurrencyInstance().format(100000)); //method chaining
+
+        //Percentage
+        System.out.println(NumberFormat.getPercentInstance().format(0.01));
     }
     }
