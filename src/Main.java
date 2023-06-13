8134563f7f802e703c6dc359e1c1def66d450ace
Gokul1909 <gokul@terzocloud.com>
2023-06-13 14:58:00 +0530

    Arithmetic expressions

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Arithmetic expressions
-        int a=2+3;
-        System.out.println(a);
+        //Order of Operations
 
-        //Explicit type conversion in expressions
-        float div=(float)10/3;
-        System.out.println(div);
+        //Precedence: ()>*/>+-
+        int a= 8+2*10;
+        System.out.println(a); //result is 28 as * is executed first
 
-        //Increment or decrement
-        int b=3;
-        int c=b++; //gets assigned before increment
-        int d=++b; //gets incremented before assignment
-        System.out.println(c);
-        System.out.println(d);
-        //Augmented assignment
-        int x=2;
-        x=x+2; //normal assignment
-        x+=2; //augmented assignment
-        System.out.println(x);
+        //It can be manipulated using ()
+        a=(8+2)*10;
+        System.out.println(a); //result is 100
     }
     }
