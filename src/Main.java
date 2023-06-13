cce560eeae74540138ce6939eb89488843e779eb
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:22:52 +0530

    Comparison

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
-import java.net.ServerSocket;
 import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //Mortgage calculator
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Principal:");
-        long principal=sc.nextLong();
-        System.out.print("\nAnnual rate of interest:");
-        float rate=sc.nextFloat();
-        rate/=100;
-        rate/=12;
-        System.out.print("\nNumber of years:");
-        int year=sc.nextByte();
-        year*=12;
-        double mortgage=principal*((rate*Math.pow((1+rate),year))/(Math.pow((1+rate),year)-1));
-        System.out.print("Mortgage:"+NumberFormat.getCurrencyInstance().format(mortgage));
+        //Comparison operators
+        int x=1;
+        int y=1;
+        System.out.println(x==y);
+        System.out.println(x!=y);
+        System.out.println(x<y);
+        System.out.println(x>y);
+        System.out.println(x>=y);
+        System.out.println(x<=y);
     }
     }
