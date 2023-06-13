c76029b3866322f1fd6742c3f6705cceb6649b69
Gokul1909 <gokul@terzocloud.com>
2023-06-13 12:21:12 +0530

    Primitive-Datatypes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Primitive datatypes
+        byte smallVal=10; //Size: 1 byte , Range :-127 to 127
+        short principleAmount=10000; //Size: 2 bytes, Range :-32K to 32K
+        int salary=1_000_000; //Size: 4 bytes, Range : -2B to 2B
+        float decimalNumber= 10.11F; //Size: 4 bytes
+        long subscribersCount=3000000; //Size: 8 bytes
+        double largeDecimal= 123.384857; //Size: 8 bytes
+        }
+    }
