91ebae3163dd3a20007608ce2669dd55a9614909
Gokul1909 <gokul@terzocloud.com>
2023-06-13 15:11:41 +0530

    Casting

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Order of Operations
+        //Casting
 
-        //Precedence: ()>*/>+-
-        int a= 8+2*10;
-        System.out.println(a); //result is 28 as * is executed first
+        //Implicit conversion
+        int x=3;
+        float y=x+1.0F; //java implicitly converts to float
+        System.out.println(y);
 
-        //It can be manipulated using '()'
-        a=(8+2)*10;
-        System.out.println(a); //result is 100
+        //Explicit conversion
+        float a=12.3F;
+        int b=(int)a+1; //the variable a is converted from float to int
+        System.out.println(b);
+
+        //String to number using classes
+        String number="1";
+        System.out.println(Integer.parseInt(number));
+        String number1="1.12";
+        System.out.println(Double.parseDouble(number1));
     }
     }
