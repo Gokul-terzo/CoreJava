5aed8e534bdd9f224aecb639139698028719b9a4
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:43:30 +0530

    Switch Statement

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //Ternary Operator
-        int age=19;
-        boolean isEligible=age>=18?true:false;
-        System.out.println(isEligible);
+        //Switch statement
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your role:");
+        String role=sc.next();
+        switch (role)
+        {
+            case "admin":
+                System.out.println("You have the admin privileges");
+                break;
+            default:
+                System.out.println("You are not admin");
+                break;
+        }
     }
     }
