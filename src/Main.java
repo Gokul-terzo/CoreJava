6ca2ca9d46f52b207753c18f4c9d7039649ff59c
Gokul1909 <gokul@terzocloud.com>
2023-06-13 14:36:27 +0530

    Constants and Multi-dimensional arrays

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Arrays
-        //Arrays in java have fixed size and the size cannot be changed after the initialization
-        int[] numbers=new int[5]; //older way of declaring the array
-        //initializing array
-        numbers[0]=1;
-        System.out.println(Arrays.toString(numbers)); //To print the array contents as printing using array name will show address
+        //Multi-dimensional arrays
 
-        int[] number={1,2,5,4}; //declaration without size
-        Arrays.sort(number); //A method of array class to sort the array in ascending order
-        System.out.println(Arrays.toString(number));
+        //2D arrays
+        int [][] two_d=new int[2][3]; //declaration with size
+        int[][] two_d1={{1,2,3},{4,5,6}}; //declaration with values
+        System.out.println(Arrays.deepToString(two_d)); //to convert a multi-dimensional array to printable string
+        System.out.println(Arrays.deepToString(two_d1));
+
+        //3D arrays
+        int [][][] three_d=new int[1][2][3];
+        System.out.println(Arrays.deepToString(three_d));
+
+        //Constants
+        final float g=9.81F; //To declare this variable as a constant i.e its value cannot be changed in further code.
+
     }
     }
