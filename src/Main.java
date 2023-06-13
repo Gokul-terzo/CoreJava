2ad1397175f118d759a9859f4dbd4da2d213d7f2
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:28:20 +0530

    Logical operations

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //Comparison operators
-        int x=1;
-        int y=1;
-        System.out.println(x==y);
-        System.out.println(x!=y);
-        System.out.println(x<y);
-        System.out.println(x>y);
-        System.out.println(x>=y);
-        System.out.println(x<=y);
+        //Logical Operators
+        boolean hasGoodMarks=true;
+        boolean hasRelevantSkills=false;
+        boolean hasArrear=false;
+        boolean canBePlaced= (hasGoodMarks||hasRelevantSkills)&& !hasArrear;
+        System.out.println(canBePlaced);
     }
     }
