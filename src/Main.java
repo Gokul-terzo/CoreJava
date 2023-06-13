32579c357012479112fdd0dad75f626ad9f1de23
Gokul1909 <gokul@terzocloud.com>
2023-06-13 17:04:42 +0530

    Break and continue

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //Do-While loop
+        //Break and continue
         Scanner sc=new Scanner(System.in);
         String input="";
         do{
             System.out.println("Enter the input");
             input=sc.next();
+            if(input.equals("pass"))
+                continue;
+            if(input.equals("quit")||input.equals("Quit"))
+                break;
             System.out.println("Input is:"+input);
         }while(!input.equals("quit")&&!input.equals("Quit"));
     }
