85626e3d5900bc35db47bb422f510979d9478bad
Gokul1909 <gokul@terzocloud.com>
2023-06-13 14:22:44 +0530

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
-import java.awt.*;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Escape sequence
+        //Arrays
+        //Arrays in java have fixed size and the size cannot be changed after the initialization
+        int[] numbers=new int[5]; //older way of declaring the array
+        //initializing array
+        numbers[0]=1;
+        System.out.println(Arrays.toString(numbers)); //To print the array contents as printing using array name will show address
 
-        // '\' can be used to escape a special character succeeding it
-        System.out.println("Hello \"Gokul\"");
-        // To escape a backslash itself we can use another preceding it
-        System.out.println("C:\\Documents\\CoreJava.txt");
-
-        //Other escape sequences commonly used are \n and \t
-        System.out.println("\n\nHi");
-        System.out.println("\t\tHi");
+        int[] number={1,2,5,4}; //declaration without size
+        Arrays.sort(number); //A method of array class to sort the array in ascending order
+        System.out.println(Arrays.toString(number));
     }
     }
