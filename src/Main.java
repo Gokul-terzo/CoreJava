57f20c5c41f51f0f24dd206f3f71b9ffe81c883a
Gokul1909 <gokul@terzocloud.com>
2023-06-13 16:39:00 +0530

    Ternary Operator

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,9 @@ import java.text.NumberFormat;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //If statement
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter today's temperature:");
-        int temp=sc.nextInt();
-        if(temp>30)
-            System.out.println("It is a hot day\nDrink a lot of water");
-        else if(temp>=20&&temp<=30)
-            System.out.println("It is a nice day");
-        else
-            System.out.println("It is cold");
+        //Ternary Operator
+        int age=19;
+        boolean isEligible=age>=18?true:false;
+        System.out.println(isEligible);
     }
     }
